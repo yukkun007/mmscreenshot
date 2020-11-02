3c7ab5fb894dcfc0231b6772e838cb28994e6be6
kato.yutaka <kato.yutaka@gmail.com>
2020-11-02 22:25:06 +0900

    readmeとCLI周りを整理

--- mmscreenshot/cli.py
+++ mmscreenshot/cli.py
@@ -1,40 +1,25 @@
 import argparse
 import logging
+import setenv
+from mmscreenshot.core import screenshot
 
 
 def main():
     parser = argparse.ArgumentParser(
         description="""
-    HTMLの一部分のスクリーンショットを撮ります。
+    HTMLのスクリーンショットを取得します。
     """
     )
 
+    parser.add_argument("url", help="取得ページのURL")
+    parser.add_argument("xpath", help="取得するHTML要素のXPath")
+    parser.add_argument("-o", "--out", help="スナップショットの出力先ファイルパス", default="./out.png")
     parser.add_argument(
-        "-m", "--mode", help="検索モードを指定します", choices=["rental", "expire", "reserve", "prepare"]
+        "-e", "--env", help=".envファイルのパスを指定します (デフォルトはカレントディレクトリの.env)", default=".env"
     )
-    parser.add_argument("-u", "--users", help="検索対象のユーザを名前(name)で指定します", nargs="*")
-    parser.add_argument(
-        "-a", "--alluser", help="全ユーザを対象に検索します(--users指定は無効になります)", action="store_true"
-    )
-    parser.add_argument(
-        "-z",
-        "--zero",
-        help="結果0件の場合の表示モードを指定します",
-        default="always",
-        choices=["always", "message", "none"],
-    )
-    parser.add_argument("-r", "--result_type", help="取得する結果の形式を指定します", choices=["message", "info"])
-    parser.add_argument("-s", "--separate", help="結果をユーザごとに個別表示します", action="store_true")
     parser.add_argument("-d", "--debug", help="デバッグログ出力をONにします", action="store_true")
-    parser.add_argument("-l", "--userlist", help="登録済みユーザのリストを表示します", action="store_true")
 
     args = parser.parse_args()
-
-    # userlistの処理
-    # returnする
-    if args.userlist:
-        pass
-        return
 
     # log設定
     formatter = "%(asctime)s : %(levelname)s : %(message)s"
@@ -44,14 +29,11 @@ def main():
     else:
         logging.basicConfig(format=formatter)
 
-    if args.mode == "rental" or args.mode == "expire":
-        # 借りてる系
-        pass
-    elif args.mode == "reserve" or args.mode == "prepare":
-        # 予約系
-        pass
-    else:
+    if setenv.load(args.env) is False:
         parser.print_help()
+        return
+
+    screenshot(args.url, args.xpath, args.out)
 
 
 if __name__ == "__main__":
